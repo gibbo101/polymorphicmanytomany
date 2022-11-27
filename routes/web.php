3820cdc30873b71a1dec27f48fa5bbe548c62992
gibbo101 <lukegibson101@gmail.com>
2022-11-27 22:55:55 +0000

    create

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+use App\Models\Video;
+use App\Models\Tag;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +19,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function() {
+  $post = Post::create(['name'=>'My first post']);
+  $tag1 = Tag::find(1);
+  $post->tags()->save($tag1);
+
+  $video = Video::create(['name'=>'video.mov']);
+  $tag2 = Tag::find(2);
+  $video->tags()->save($tag2);
+
 });
